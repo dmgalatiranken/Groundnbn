3859ebca0ba9c139b13068ced5a906252a7c27ef
Dominic Galati <Dominic_Galati@insideranken.org>
2025-05-12 17:00:52 -0500

    Project Initialization
    Project Initialization

---
+++ MVCPortfolioProject/Groundnbn/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Groundnbn.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
